4597ee552d1bef30b1a65cd293f260d9a12e607d
John Pickett <pickett.bill@gmail.com>
2026-09-17 08:21:24 -0230

    remove tests

--- tests/tests/chapter_8/invalid_parse/missing_for_header_semicolon.c
+++
@@ -1,5 +0,0 @@
-int main(void) {
-    for (int i = 0; i < 10)
-        ;
-    return 0;
-}
--- tests/tests/chapter_8/valid/for_decl_no_init.c
+++
@@ -1,6 +0,0 @@
-int main(void) {
-    int x = 4;
-    for (int i; (i = x - 2); x = i + 1) {
-    }
-    return x;
-}
